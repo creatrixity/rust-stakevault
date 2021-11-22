b930a7d5a0bff77cf3231a1d2aa035f0237ed80f
Caleb Mathew <creatrixity@gmail.com>
2021-11-22 20:37:40 +0100

    feat(account-creation): add runtime env vars

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read application configuration");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))    
-        .connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to PostgreSQL");
+        .connect_lazy_with(configuration.database.with_db());
 
     let address: &str = &format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = std::net::TcpListener::bind(address)?;
